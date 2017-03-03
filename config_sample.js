2b6c6dfa2851910e715832b504a1b2e4b81c54be
Duncan Gibbs <duncgibbs@gmail.com>
2017-03-03 08:58:08 -0600

    updates config_sample
    whoops missed a comma

--- config_sample.js
+++ config_sample.js
@@ -1,5 +1,9 @@
 // copy this to config.js and update with desired values
+// The future_api_id is the API id for Future. You can change this if you want to bother a different artist.
+// But why would you want to do that?
 module.exports = {
-    access_token: '[RAP_GENIUS_API_ACCESS_TOKEN]',
+    client_id: '[GENIUS_API_CLIENT_ID]',
+    client_secret: '[GENIUS_API_CLIENT_SECRET]',
+    redirect_uri: '[GENIUS_API_CLIENT_REDIRECT_URI]',
     future_api_id: 2197
 }
